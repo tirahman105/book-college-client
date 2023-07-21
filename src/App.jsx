ca103705f50750c23940509505a767f61bb6ff6d
tirahman105 <tirahman105@gmail.com>
2023-07-22 00:24:34 +0600

    initial commit react router installed, tailwind and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+       
+      <h1>Vite + React</h1>
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
+     
+    </>
+  )
+}
+
+export default App
