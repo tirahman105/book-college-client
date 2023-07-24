0b9ce09d10bfd2816b7bf325ca5f6534dfd5d04b
tirahman105 <tirahman105@gmail.com>
2023-07-24 10:29:22 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/NavBar/NavBar";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 
 
@@ -9,6 +10,7 @@ const Main = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
